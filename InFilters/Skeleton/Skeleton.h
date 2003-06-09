ee6acbd6d4741a0718245f9cd0346a3c601df2e5
Andrew Ruder <andy@aeruder.net>
2003-06-09 20:16:10 +0000

    Skeleton
    git-svn-id: file:///Users/andy/svnstuff/cvsroot/TalkSoup-svn/trunk@144 475b2e0a-8226-4799-b600-e189232e4290

---
+++ InFilters/Skeleton/Skeleton.h
@@ -0,0 +1,31 @@
+/***************************************************************************
+                             Skeleton.h
+                          -------------------
+    begin                : Sat May 10 18:58:30 CDT 2003
+    copyright            : (C) 2003 by Andy Ruder
+    email                : aeruder@yahoo.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+@class Skeleton;
+
+#ifndef SKELETON_H
+#define SKELETON_H
+
+#include <Foundation/NSObject.h>
+
+@class NSAttributedString;
+
+@interface Skeleton : NSObject
+// Open up TalkSoupProtocols.h and insert methods you want here...
+@end
+
+#endif
