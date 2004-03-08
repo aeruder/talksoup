3874a133a57776ffd3f21de224f2d22b83188d53
Andrew Ruder <andy@aeruder.net>
2004-03-08 01:55:15 +0000

    Everything is horribly broked, but I don't care
    git-svn-id: file:///Users/andy/svnstuff/cvsroot/TalkSoup-svn/trunk@390 475b2e0a-8226-4799-b600-e189232e4290

--- Output/GNUstepOutput/Controllers/ContentControllers/Tab/TabContentController.h
+++ Output/GNUstepOutput/Controllers/ContentControllers/Tab/TabContentController.h
@@ -1,7 +1,7 @@
 /***************************************************************************
-                        TabContentController.h
+                                TabContentController.h
                           -------------------
-    begin                : Sat Jan 18 01:38:06 CST 2003
+    begin                : Tue Jan 20 22:08:40 CST 2004
     copyright            : (C) 2003 by Andy Ruder
     email                : aeruder@yahoo.com
  ***************************************************************************/
@@ -15,106 +15,12 @@
  *                                                                         *
  ***************************************************************************/
 
-@class TabContentController, NSString;
+@class TabContentController;
 
 #ifndef TAB_CONTENT_CONTROLLER_H
 #define TAB_CONTENT_CONTROLLER_H
 
 #import <Foundation/NSObject.h>
-#import <Foundation/NSMapTable.h>
 
-@class NSTextField, NSTabView, NSWindow;
-@class NSMutableDictionary, NSString, NSAttributedString;
-@class NSTabViewItem, NSArray;
-@class NSColor;
-@class ConnectionController, NSFont;
-
-NSAttributedString *SubstituteColorCodesIntoAttributedString(NSAttributedString *str);
-
-@interface ContentController : NSObject
-{
-	NSTextField *typeView;
-	NSTextField *nickView;
-	NSTabView *tabView;
-	NSWindow *window;
-	NSMutableDictionary *nameToChannel;
-	NSMutableDictionary *nameToQuery;
-	NSMutableDictionary *nameToBoth;
-	NSMutableDictionary *nameToPresentation;
-	NSMutableDictionary *nameToLabel;
-	NSMutableDictionary *nameToTabItem;
-	NSMutableArray *highlightedTabs;
-	NSMapTable *tabItemToName;
-	NSMapTable *bothToName;
-	NSString *current;
-	ConnectionController *connection;
-	NSFont *chatFont;
-}
-- initWithConnectionController: (ConnectionController *)connect;
-
-- (ConnectionController *)connectionController;
-- setConnectionController: (ConnectionController *)aConnectionController;
-
-- updatedColor: (NSString *)aKey old: (NSString *)oldValue;
-- (NSColor *)colorForKey: (NSString *)aKey;
-
-- highlightTabWithName: (NSString *)aName withColor: (NSString *)aColor
-   withPriority: (BOOL)prio;
-
-- (NSArray *)allViews;
-- (NSArray *)allChannelNames;
-- (NSArray *)allQueryNames;
-
-- (BOOL)isQueryName: (NSString *)aName;
-- (BOOL)isChannelName: (NSString *)aName;
-
-- (NSTextField *)typeView;
-
-- (NSTextField *)nickView;
-
-- (NSTabView *)tabView;
-
-- (NSWindow *)window;
-
-- (NSAttributedString *)labelForViewWithName: (NSString *)aChannel;
-
-- (NSString *)presentationNameForViewWithName: (NSString *)aChannel;
-
-- (id)controllerForViewWithName: (NSString *)aChannel;
-
-- (NSTabViewItem *)tabViewItemForViewWithName: (NSString *)aChannel;
-
-- (NSString *)viewNameForTabViewItem: (NSTabViewItem *)aItem;
-
-- (NSString *)viewNameForController: controller;
-
-- putMessage: (id)aString in: (id)aChannel withEndLine: (BOOL)end;
-
-- putMessage: (id)aString in: (id)aChannel;
-
-- putMessageInAll: (id)aString withEndLine: (BOOL)end;
-
-- putMessageInAll: (id)aString;
-
-- addQueryWithName: (NSString *)aName withLabel: (NSAttributedString *)aLabel;
-
-- addChannelWithName: (NSString *)aName withLabel: (NSAttributedString *)aLabel;
-
-- setLabel: (NSAttributedString *)aLabel forViewWithName: (NSString *)aName;
-
-- closeViewWithName: (NSString *)aName;
-
-- renameViewWithName: (NSString *)aName to: (NSString *)newName;
-
-- focusViewWithName: (NSString *)aName;
-
-- (NSString *)currentViewName;
-
-- setNickViewString: (NSString *)aNick;
-
-- setChatFont: (NSFont *)aFont;
-
-- (NSFont *)chatFont;
-@end
-
+@interface TabContentController : NSObject
 #endif
